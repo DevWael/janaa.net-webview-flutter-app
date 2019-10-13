f177419abe3e52be2092df49df04a37352a85aed
Wael <dev.ahmedwael@gmail.com>
2019-10-13 15:13:54 +0200

    added app drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyA
+pp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'JanaStore'),
+      home: MyHomePage(title: 'Jana'),
     );
   }
 }
@@ -44,10 +45,10 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.home),
               onPressed: go_to_home,
             ),
-            IconButton(
-              icon: Icon(Icons.shop),
-              onPressed: go_to_shop,
-            ),
+//            IconButton(
+//              icon: Icon(Icons.shop),
+//              onPressed: go_to_shop,
+//            ),
             IconButton(
               icon: Icon(Icons.shopping_cart),
               onPressed: go_to_cart,
@@ -56,11 +57,54 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.supervisor_account),
               onPressed: go_to_myaccount,
             ),
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: reload_page,
-            ),
+//            IconButton(
+//              icon: Icon(Icons.refresh),
+//              onPressed: reload_page,
+//            ),
           ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                  Colors.green,
+                  Colors.lightGreenAccent,
+                ])),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Material(
+                        elevation: 5,
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                        child: Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Image.asset(
+                            'images/logo.png',
+                            width: 60,
+                            height: 60,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Jana Store',
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              CustomListTile(Icons.person, 'profile', () => {}),
+              CustomListTile(Icons.person, 'profile', () => {}),
+              CustomListTile(Icons.person, 'profile', () => {}),
+              CustomListTile(Icons.person, 'profile', () => {}),
+              CustomListTile(Icons.person, 'profile', () => {}),
+            ],
+          ),
         ),
         body: WebView(
           key: _key,
@@ -91,5 +135,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void go_to_home() {
     _webController.loadUrl(_url);
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  IconData icon;
+  String text;
+  Function onTap;
+
+  CustomListTile(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey.shade300))),
+        child: InkWell(
+          splashColor: Colors.lightGreenAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        text,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
