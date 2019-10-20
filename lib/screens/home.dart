cfaac924aeea07e667930bfa602ce9f14f3fe91f
Ahmad Wael <dev.ahmedwael@gmail.com>
2019-10-20 17:21:44 +0200

    disable reload gesture

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,11 +6,11 @@ import 'package:janastore/config.dart';
 import 'package:janastore/CustomListTile.dart';
 import 'package:janastore/prefrences.dart';
 import 'dart:io';
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:share/share.dart';
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
   SharedPreferences sharedPreferences;
   bool _testValue;
+  var refreshKey = GlobalKey<RefreshIndicatorState>();
 
   //preloader
   num _stackToView = 1;
@@ -41,9 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _stackToView = 0;
     });
   }
-
-
-
 
   _openLink(url) async {
     if (await canLaunch(url)) {
@@ -60,7 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void updateLang(bool lang) {
-
     SharedPreferences.getInstance().then((SharedPreferences sp) {
       sharedPreferences = sp;
       _testValue = sharedPreferences.getBool('');
@@ -69,14 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
       if (_testValue == null) {
         _testValue = false;
         //persist(_testValue); // set an initial value
-      }else{
-
-      }
+      } else {}
       setState(() {});
     });
 
-    setState(() {
-    });
+    setState(() {});
   }
 
   webViewHolder() {
@@ -89,12 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
       javascriptMode: JavascriptMode.unrestricted,
       userAgent: AppConfig.userAgent,
       onPageFinished: _handleLoad,
-      gestureRecognizers: Set()
-        ..add(
-          Factory<VerticalDragGestureRecognizer>(
-                () => VerticalDragGestureRecognizer(),
-          ),
-        ),
       onWebViewCreated: (WebViewController _tmpWebController) {
         _webController = _tmpWebController;
       },
@@ -108,203 +96,203 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: goToHome,
-            ),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: goToHome,
+          ),
 //            IconButton(
 //              icon: Icon(Icons.shop),
 //              onPressed: go_to_shop,
 //            ),
-            IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: goToCart,
-            ),
-            IconButton(
-              icon: Icon(Icons.supervisor_account),
-              onPressed: goToMyAccount,
-            ),
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: (){
-                Share.share(url);
-              },
-            ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: goToCart,
+          ),
+          IconButton(
+            icon: Icon(Icons.supervisor_account),
+            onPressed: goToMyAccount,
+          ),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              Share.share(url);
+            },
+          ),
 //            IconButton(
 //              icon: Icon(Icons.refresh),
 //              onPressed: reload_page,
 //            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: <Color>[
-                  Colors.green,
-                  Colors.lightGreenAccent,
-                ])),
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Material(
-                        elevation: 5,
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                        child: Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Image.asset(
-                            'images/logo.png',
-                            width: 60,
-                            height: 60,
-                          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: <Color>[
+                Colors.green,
+                Colors.lightGreenAccent,
+              ])),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Image.asset(
+                          'images/logo.png',
+                          width: 60,
+                          height: 60,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          AppConfig.appTitle,
-                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        AppConfig.appTitle,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            CustomListTile(FontAwesomeIcons.home, 'Home', () {
+              Navigator.pop(context);
+              goToHome();
+            }),
+            CustomListTile(FontAwesomeIcons.user, 'Profile', () {
+              Navigator.pop(context);
+              goToMyAccount();
+            }),
+            CustomListTile(FontAwesomeIcons.store, 'Shop', () {
+              Navigator.pop(context);
+              goToShop();
+            }),
+            CustomListTile(FontAwesomeIcons.shoppingBasket, 'Cart', () {
+              Navigator.pop(context);
+              goToCart();
+            }),
+            CustomListTile(FontAwesomeIcons.cog, 'Settings', () {
+              Navigator.pop(context);
+              Navigator.of(context).pushNamed('/settings');
+            }),
+            CustomListTile(FontAwesomeIcons.exclamationCircle, 'Exit', () {
+              exit(0);
+            }),
+            Container(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Container(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      InkWell(
+                          onTap: () {
+                            _openLink(AppConfig.facebook);
+                          },
+                          child: Icon(FontAwesomeIcons.facebookSquare,
+                              color: hexToColor(AppConfig.facebookColor),
+                              size: 30.0)),
+                      InkWell(
+                        onTap: () {
+                          _openLink(AppConfig.whatsapp);
+                        },
+                        child: Icon(FontAwesomeIcons.whatsappSquare,
+                            color: hexToColor(AppConfig.whatsAppColor),
+                            size: 30.0),
+                      ),
+                      InkWell(
+                          onTap: () {
+                            _openLink(AppConfig.instagram);
+                          },
+                          child: Icon(FontAwesomeIcons.instagram,
+                              color: hexToColor(AppConfig.instagramColor),
+                              size: 30.0)),
+                      InkWell(
+                        onTap: () {
+                          _openLink(AppConfig.youtube);
+                        },
+                        child: Icon(FontAwesomeIcons.youtubeSquare,
+                            color: hexToColor(AppConfig.youtubeColor),
+                            size: 30.0),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          _openLink('https://maroof.sa');
+                        },
+                        child: Image.asset(
+                          'images/maroof_logo.png',
+                          width: 30,
+                          height: 30,
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
               ),
-              CustomListTile(FontAwesomeIcons.home, 'Home', () {
-                Navigator.pop(context);
-                goToHome();
-              }),
-              CustomListTile(FontAwesomeIcons.user, 'Profile', () {
-                Navigator.pop(context);
-                goToMyAccount();
-              }),
-              CustomListTile(FontAwesomeIcons.store, 'Shop', () {
-                Navigator.pop(context);
-                goToShop();
-              }),
-              CustomListTile(FontAwesomeIcons.shoppingBasket, 'Cart', () {
-                Navigator.pop(context);
-                goToCart();
-              }),
-              CustomListTile(FontAwesomeIcons.cog, 'Settings', () {
-                Navigator.pop(context);
-                Navigator.of(context).pushNamed('/settings');
-              }),
-              CustomListTile(FontAwesomeIcons.exclamationCircle, 'Exit', () {
-                exit(0);
-              }),
-              Container(
-                child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                    padding: EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        InkWell(
-                            onTap: () {
-                              _openLink(AppConfig.facebook);
-                            },
-                            child: Icon(FontAwesomeIcons.facebookSquare,
-                                color: hexToColor(AppConfig.facebookColor),
-                                size: 30.0)),
-                        InkWell(
-                          onTap: () {
-                            _openLink(AppConfig.whatsapp);
-                          },
-                          child: Icon(FontAwesomeIcons.whatsappSquare,
-                              color: hexToColor(AppConfig.whatsAppColor),
-                              size: 30.0),
-                        ),
-                        InkWell(
-                            onTap: () {
-                              _openLink(AppConfig.instagram);
-                            },
-                            child: Icon(FontAwesomeIcons.instagram,
-                                color: hexToColor(AppConfig.instagramColor),
-                                size: 30.0)),
-                        InkWell(
-                          onTap: () {
-                            _openLink(AppConfig.youtube);
-                          },
-                          child: Icon(FontAwesomeIcons.youtubeSquare,
-                              color: hexToColor(AppConfig.youtubeColor),
-                              size: 30.0),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            _openLink('https://maroof.sa');
-                          },
-                          child: Image.asset(
-                            'images/maroof_logo.png',
-                            width: 30,
-                            height: 30,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        body:IndexedStack(
-          index: _stackToView,
-          children: [
-            Column(
-              children: < Widget > [
-                Expanded(
-                    child: WebView(
-                      key: _key,
-                      initialUrl: url,
-                      javascriptMode: JavascriptMode.unrestricted,
-                      userAgent: AppConfig.userAgent,
-                      onPageFinished: _handleLoad,
-                      onWebViewCreated: (WebViewController _tmpWebController) {
-                        _webController = _tmpWebController;
-//                        updareCurrentUrl(_webController.currentUrl());
-                      },
-                      gestureRecognizers: Set()
-                        ..add(Factory<VerticalDragGestureRecognizer>(() {
-                          return VerticalDragGestureRecognizer()
-                            ..onStart = (DragStartDetails details) {
-                              print("darg start");
-                            }
-                            ..onUpdate = (DragUpdateDetails details) {
-                              print("Drag update: $details");
-                            }
-                            ..onDown = (DragDownDetails details) {
-                              reloadPage();
-                            }
-                            ..onCancel = () {
-                              print("Drag cacel");
-                            }
-                            ..onEnd = (DragEndDetails details) {
-                              print("Drag end");
-                            };
-                        })),
-                    )
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.white,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
             ),
           ],
-        )
+        ),
+      ),
+      body: IndexedStack(
+        index: _stackToView,
+        children: [
+          Column(
+            children: <Widget>[
+              Expanded(
+                  child: WebView(
+                key: _key,
+                initialUrl: url,
+                javascriptMode: JavascriptMode.unrestricted,
+                userAgent: AppConfig.userAgent,
+                onPageFinished: _handleLoad,
+                onWebViewCreated: (WebViewController _tmpWebController) {
+                  _webController = _tmpWebController;
+//                        updareCurrentUrl(_webController.currentUrl());
+                },
+//                gestureRecognizers: Set()
+//                  ..add(Factory<VerticalDragGestureRecognizer>(() {
+//                    return VerticalDragGestureRecognizer()
+//                      ..onStart = (DragStartDetails details) {
+//                        print("darg start");
+//                      }
+//                      ..onUpdate = (DragUpdateDetails details) {
+//                        print("Drag update: $details");
+//                      }
+//                      ..onDown = (DragDownDetails details) {
+//                        print("Drag reload");
+//                        reloadPage();
+//                      }
+//                      ..onCancel = () {
+//                        print("Drag cacel");
+//                      }
+//                      ..onEnd = (DragEndDetails details) {
+//                        print("Drag end");
+//                      };
+//                  })),
+              )),
+            ],
+          ),
+          Container(
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
     );
   }
-//webViewHolder()
 
+//webViewHolder()
 
   void reloadPage() {
     setState(() {
